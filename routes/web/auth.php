3902e7c7b402dad387cfe358d11502e9c9ba11f5
Alexandre Ferreira <shieldforce2@gmail.com>
2024-03-23 13:45:56 -0300

    segmentou em arquivos separados os grupos de rotas!

---
+++ routes/web/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->controller(ProfileController::class)->group(function () {
+
+    Route::get('/profile', 'edit')
+         ->name('profile.edit');
+
+    Route::patch('/profile', 'update')
+         ->name('profile.update');
+
+    Route::delete('/profile', 'destroy')
+         ->name('profile.destroy');
+
+});
